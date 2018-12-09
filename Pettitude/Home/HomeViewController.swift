eef5f714551ef0ea071dd1be984131c9ddb3771a
Arthur Papailhau <papailhau@gmail.com>
2018-12-09 17:21:37 +0100

    Setup Vision

--- Pettitude/Home/HomeViewController.swift
+++ Pettitude/Home/HomeViewController.swift
@@ -17,20 +17,51 @@ protocol HomePresentableListener: class {
     // interactor class.
 }
 
-final class HomeViewController: UIViewController, HomePresentable, HomeViewControllable {
+final class HomeViewController: UIViewController, HomePresentable, HomeViewControllable, ARSKViewDelegate, ARSessionDelegate {
 
     weak var listener: HomePresentableListener?
     
-    private lazy var internalView: HomeView = {
-        return HomeView()
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view = internalView
+        setupHome()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
     }
     
     // MARK: - Private
+    
+    private func setupHome() {
+        setupVision()
+    }
+    
+    private func setupVision() {
+        sceneView = ARSKView(frame: view.frame)
+        view.addSubview(sceneView)
+        
+        let overlayScene = SKScene()
+        overlayScene.scaleMode = .aspectFill
+        sceneView.delegate = self
+        sceneView.presentScene(overlayScene)
+        sceneView.session.delegate = self
+    }
+    
+    // MARK: - Protocol ARSessionDelegate
+    
+    // The pixel buffer being held for analysis; used to serialize Vision requests.
+    private var currentBuffer: CVPixelBuffer?
+    // Queue for dispatching vision classification requests
+    private let visionQueue = DispatchQueue(label: "com.example.apple-samplecode.ARKitVision.serialVisionQueue")
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        guard currentBuffer == nil, case .normal = frame.camera.trackingState else {
+            return
+        }
+        self.currentBuffer = frame.capturedImage
+    }
     
     private var sceneView: ARSKView!
 }
